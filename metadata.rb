4016f589b8e5f0e9201b37acfb8fdfa952806ec1
Chris Gianelloni <cgianelloni@applause.com>
2018-11-07 18:56:49 -0500

    Version bump to 0.0.4
    Signed-off-by: Chris Gianelloni <cgianelloni@applause.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cgianelloni@applause.com'
 license          'Apache-2.0'
 description      'Installs and configures strongDM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.3'
+version          '0.0.4'
 
 depends 'ark', '~> 3.1.1'
 
