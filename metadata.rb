2516ec2870061bf280cb8a64f8657fb30b5adc74
Chris Gianelloni <cgianelloni@applause.com>
2019-04-23 18:03:20 -0400

    Release v0.5.7
    Signed-off-by: Chris Gianelloni <cgianelloni@applause.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@applause.com'
 license          'Apache-2.0'
 description      'Installs and configures strongDM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.6'
+version          '0.5.7'
 
 %w(redhat centos scientific amazon ubuntu debian suse).each do |os|
   supports os
@@ -12,4 +12,4 @@ end
 
 source_url 'https://github.com/ApplauseOSS/strongdm_cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/ApplauseOSS/strongdm_cookbook/issues' if respond_to?(:issues_url)
-chef_version '>= 12.7' if respond_to?(:chef_version) # ark dependency
+chef_version '>= 12.14' if respond_to?(:chef_version)
